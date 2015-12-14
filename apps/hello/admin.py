b726e39cb55c7122e621f3f1a10930b68c3c4567
pavlopython <pavlopython@ukr.net>
2015-12-15 01:30:32 +0200

    commit new branch

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.hello.models import BIO
 
-# Register your models here.
+
+class AdminBIO(admin.ModelAdmin):
+    pass
+
+admin.site.register(BIO, AdminBIO)
+
--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class BIO(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    birth = models.DateField(verbose_name='You first see this world in')
+    biography = models.TextField()
+    phone = models.CharField(max_length=12)
+    skype = models.CharField(max_length=25)
+    other = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'bio'
