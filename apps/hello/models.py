f8d802d64a76aab28919f3100109d2b8f0e59ab5
pavlopython <pavlopython@ukr.net>
2015-12-15 14:22:29 +0200

    create fixtured

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -6,10 +6,10 @@ class BIO(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     birth = models.DateField(verbose_name='You first see this world in')
-    biography = models.TextField()
-    phone = models.CharField(max_length=12)
-    skype = models.CharField(max_length=25)
-    other = models.CharField(max_length=255)
+    biography = models.TextField(blank=True)
+    phone = models.CharField(max_length=12, blank=True)
+    skype = models.CharField(max_length=25, blank=True)
+    other = models.CharField(max_length=255, blank=True)
 
     class Meta:
         db_table = 'bio'
